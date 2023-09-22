a5ad244c735045530c2f1419a82fdafb42531034
rameshbb34 <65815554+rameshbb34@users.noreply.github.com>
2023-09-22 14:34:44 +0530

    signup page

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div>
+      <form>
+        <div>
+          <label>Name</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Email</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Contact</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Addres</label>
+          <input></input>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
